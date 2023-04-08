aac696b71debb8e091e810905080a398586086a9
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-08 16:08:21 +0700

    edit profile

--- app/src/main/java/com/example/dattingapp/FillProfileActivity.java
+++ app/src/main/java/com/example/dattingapp/FillProfileActivity.java
@@ -3,20 +3,68 @@ package com.example.dattingapp;
 import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FillProfileActivity  extends AppCompatActivity {
-    EditText dateEditText;
+    EditText editTextDateOfBirth;
+    Spinner  spnGender;
     int year, month, day;
+    List<String> listGender;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fill_profile);
         Mapping();
         SetListener();
+        SetData();
+        BindingData();
+
+
+    }
+
+    private void BindingData() {
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item,
+                listGender);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnGender.setAdapter(adapter);
+        spnGender.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                onItemSelectedHandler(parent, view, position, id);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private void onItemSelectedHandler(AdapterView<?> parent, View view, int position, long id) {
+        Adapter adapter = parent.getAdapter();
+        String gender = (String)adapter.getItem(position);
+        Toast.makeText(getApplicationContext(), "Selected Employee: " +gender ,Toast.LENGTH_SHORT).show();
+
+    }
+
+    private void SetData() {
+        listGender = new ArrayList<>();
+        listGender.add("Male");
+        listGender.add("Female");
+        listGender.add("None");
     }
 
     public void showDatePickerDialog(View v) {
@@ -28,26 +76,25 @@ public class FillProfileActivity  extends AppCompatActivity {
         public void onDateSet(DatePicker datePicker, int year, int month, int day) {
             // Do something with the date chosen by the user
             String date = day + "/" + (month + 1) + "/" + year;
-            dateEditText.setText(date);
+            editTextDateOfBirth.setText(date);
         }
     };
 
     private  void Mapping(){
-        dateEditText= findViewById(R.id.editTextDateOfBirth);
+        editTextDateOfBirth = findViewById(R.id.editTextDateOfBirth);
+        spnGender = findViewById(R.id.spinnerGender);
     }
     private  void SetListener(){
-        dateEditText.setOnClickListener(new View.OnClickListener() {
+        editTextDateOfBirth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showDatePickerDialog(v);
             }
         });
-        dateEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        editTextDateOfBirth.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if(hasFocus){
-                    showDatePickerDialog(v);
-                }
+            public void onClick(View v) {
+                showDatePickerDialog(v);
             }
         });
     }
