d4bfb453369713a4463e6f1c25998d2cbeb4f4f4
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-08 22:08:27 +0700

    add add picture

---
+++ app/src/main/java/com/example/dattingapp/AddPictureActivity.java
@@ -0,0 +1,12 @@
+package com.example.dattingapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddPictureActivity extends AppCompatActivity {
+    protected void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_picture);
+    }
+}
