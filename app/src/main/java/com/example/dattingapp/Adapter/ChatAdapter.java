6c780619c77acad4e288bb90bfdb94d3a22d45d2
PainCodermax <trungafk0806@gmail.com>
2023-04-14 21:12:38 +0700

    add clicklisten

--- app/src/main/java/com/example/dattingapp/Adapter/ChatAdapter.java
+++ app/src/main/java/com/example/dattingapp/Adapter/ChatAdapter.java
@@ -1,5 +1,8 @@
 package com.example.dattingapp.Adapter;
 
+import static androidx.core.content.ContextCompat.startActivity;
+
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -7,7 +10,8 @@ import android.widget.ImageView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
+import com.example.dattingapp.ChatMessageActivity;
+import com.example.dattingapp.MainActivity;
 import com.example.dattingapp.R;
 
 import java.util.List;
@@ -26,8 +30,20 @@ public class ChatAdapter  extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
             super(itemView);
 
             imageViewAvatar = itemView.findViewById(R.id.imageViewAvatar);
+
+            itemView.setOnClickListener(new View.OnClickListener(){
+
+                public void onClick(View v) {
+
+                    Intent intent = new Intent(v.getContext(), ChatMessageActivity.class);
+                    v.getContext().startActivity(intent);
+                }
+            });
+
         }
+
     }
+
 
     @Override
     public ChatAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -40,11 +56,15 @@ public class ChatAdapter  extends RecyclerView.Adapter<ChatAdapter.ViewHolder> {
     public void onBindViewHolder(ViewHolder holder, int position) {
 
         holder.imageViewAvatar.setImageResource(avatarList.get(position));
+
     }
 
     @Override
     public int getItemCount() {
         return avatarList.size();
     }
+
+
+
 }
 
