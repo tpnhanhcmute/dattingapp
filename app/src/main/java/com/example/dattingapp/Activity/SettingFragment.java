a9a2e392d7020525cfcb70fae1fafc651d127f23
tpnhan12a1 <tpnhan12a1@gmail.com>
2023-04-28 14:27:33 +0700

    fix bug  login

---
+++ app/src/main/java/com/example/dattingapp/Activity/SettingFragment.java
@@ -0,0 +1,15 @@
+package com.example.dattingapp.Activity;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.dattingapp.R;
+
+public class Setting extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_setting);
+    }
+}
